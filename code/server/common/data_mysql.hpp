f6237fd98c61d5a91c2df8b80e86ea944c306a71
ULookup <CHBulookup@outlook.com>
2025-12-19 21:37:26 +0800

    fix: 修改了命名拼写错误

--- code/server/common/data_mysql.hpp
+++ code/server/common/data_mysql.hpp
@@ -14,7 +14,7 @@ class ODBFactory
 {
 public:
     static std::shared_ptr<odb::core::database> create(const std::string &user,
-                                                    const std::string &passwrod,
+                                                    const std::string &password,
                                                     const std::string &host,
                                                     const std::string &db,
                                                     const std::string &cset,
@@ -22,7 +22,7 @@ public:
                                                     int conn_pool_count) 
     {
        std::unique_ptr<odb::mysql::connection_pool_factory> factory(new odb::mysql::connection_pool_factory(conn_pool_count, 0));
-       auto res = std::make_shared<odb::mysql::database>(user, passwrod, db, host, port, "", cset, 0, std::move(factory));
+       auto res = std::make_shared<odb::mysql::database>(user, password, db, host, port, "", cset, 0, std::move(factory));
        return res;
     }
 };
