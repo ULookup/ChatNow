10eacb4c610484c2ef273e3d17e271f71ea3b4cd
ULookup <CHBulookup@outlook.com>
2025-12-26 22:10:05 +0800

    feat: 好友关系表的ODB定义

---
+++ code/server/odb/relation.hxx
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+#include <cstddef>
+#include <odb/nullable.hxx>
+#include <odb/core.hxx>
+
+namespace chatnow
+{
+
+#pragma db object table("relation")
+class Relation
+{
+public:
+    Relation() = default;
+    Relation(const std::string &uid, const std::string &pid)
+        : _user_id(uid), _peer_id(pid) {}
+    
+    std::string user_id() const { return _user_id; }
+    void user_id(const std::string &uid) { _user_id = uid; }
+
+    std::string peer_id() const { return _peer_id; }
+    void peer_id(const std::string &pid) { _peer_id = pid; }
+private:
+    friend class odb::access;
+    #pragma db id auto
+    unsigned long _id;
+    #pragma db type("varchar(64)") index
+    std::string _user_id;
+    #pragma db type("varchar(64)")
+    std::string _peer_id;
+};
+
+}
