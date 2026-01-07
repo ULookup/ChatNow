d782160685fc18ca073a8c6aa6782e4728a3ea33
ULookup <CHBulookup@outlook.com>
2026-01-07 20:05:36 +0800

    feat: 完成聊天会话管理子服务源文件的创建，准备从好友管理子服务拆分处理

---
+++ code/server/chatsession/source/chatsession_server.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <brpc/server.h>
+#include <butil/logging.h>
+#include "etcd.hpp"
+#include "logger.hpp"
+#include "utils.hpp"
+#include "base.pb.h"
+
+namespace chatnow
+{
+
+
+
+} // namespace chatnow;
