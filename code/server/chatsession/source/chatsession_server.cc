6e6201b351ddbc9560786793481f024736cba8a3
ULookup <CHBulookup@outlook.com>
2026-01-18 18:43:15 +0800

    feat: 完成了会话管理子服务，并通过了单元测试，等待进一步联调

--- code/server/chatsession/source/chatsession_server.cc
+++ code/server/chatsession/source/chatsession_server.cc
@@ -1,7 +1,46 @@
 #include "chatsession_server.h"
-#include <gflags/gflags.h>
 
-int main()
+DEFINE_bool(run_mode, false, "程序的运行模式 false-调试 ; true-发布");
+DEFINE_string(log_file, "", "发布模式下，用于指定日志的输出文件");
+DEFINE_int32(log_level, 0, "发布模式下，用于指定日志的输出等级");
+
+DEFINE_string(registry_host, "http://127.0.0.1:2379", "服务注册中心地址");
+DEFINE_string(base_service, "/service", "服务监控根目录");
+DEFINE_string(instance_name, "/chatsession_service/instance", "服务监控根目录");
+DEFINE_string(access_host, "127.0.0.1:10007", "当前实例的外部访问地址");
+
+DEFINE_int32(listen_port, 10007, "RPC服务器监听端口");
+DEFINE_int32(rpc_timeout, -1, "RPC调用超时时间");
+DEFINE_int32(rpc_threads, 1, "RPC的IO线程数量");
+
+DEFINE_string(user_service, "/service/user_service", "用户管理子服务名称");
+DEFINE_string(file_service, "/service/file_service", "文件存储子服务名称");
+DEFINE_string(message_service, "/service/message_service", "消息存储子服务名称");
+
+DEFINE_string(es_host, "http://127.0.0.1:9200/", "ES搜索引擎服务器URL");
+
+DEFINE_string(mysql_host, "127.0.0.1", "MySQL服务器访问地址");
+DEFINE_string(mysql_user, "root", "MySQL访问服务器用户名");
+DEFINE_string(mysql_pswd, "YHY060403", "MySQL服务器访问密码");
+DEFINE_string(mysql_db, "chatnow", "MySQL默认库名称");
+DEFINE_string(mysql_cset, "utf8", "MySQL客户端字符集");
+DEFINE_int32(mysql_port, 0, "MySQL服务器访问端口");
+DEFINE_int32(mysql_pool_count, 4, "MySQL连接池最大连接数量");
+
+int main(int argc, char *argv[])
 {
+    google::ParseCommandLineFlags(&argc, &argv, true);
+    chatnow::init_logger(FLAGS_run_mode, FLAGS_log_file, FLAGS_log_level);
+
+    chatnow::ChatSessionServerBuilder cssb;
+    cssb.make_es_object({FLAGS_es_host});
+    cssb.make_mysql_object(FLAGS_mysql_user, FLAGS_mysql_pswd, FLAGS_mysql_host, FLAGS_mysql_db, FLAGS_mysql_cset, FLAGS_mysql_port, FLAGS_mysql_pool_count);
+    cssb.make_discovery_object(FLAGS_registry_host, FLAGS_base_service, FLAGS_user_service, FLAGS_file_service, FLAGS_message_service);
+    cssb.make_rpc_object(FLAGS_listen_port, FLAGS_rpc_timeout, FLAGS_rpc_threads);
+    cssb.make_registry_object(FLAGS_registry_host, FLAGS_base_service + FLAGS_instance_name, FLAGS_access_host);
+
+    auto server = cssb.build();
+    server->start();
+
     return 0;
 }
